4deee37bcdb07d16fd24711c72b3bc7e3a6fdc94
Dabaif <fatieydabai@gmail.com>
2022-03-11 12:29:13 -0800

    My 4-puts.c task commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+   * main - Entry point
+    *
+     * Return:0 after printing function
+     */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	    return (0);
+}
+
