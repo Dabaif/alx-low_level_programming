88dfcfa690e5b11530982b772eec84d8aa67ba43
Dabaif <fatieydabai@gmail.com>
2022-03-16 03:34:02 -0700

    0-putchar.c commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,12 @@
 #include <unistd.h>
-#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- *
- * Return: on success 1
+ *  _putchar - writes the character c to stdout
+ *    
+ * Return: On success 1.
  */
+int _putchar(char c)
+{
+		return (write(1, &c, 1));
+}
 
