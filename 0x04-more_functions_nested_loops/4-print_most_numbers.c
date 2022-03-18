16fd744cee33c9570756496ff5a9576887f24b57
Dabaif <fatieydabai@gmail.com>
2022-03-18 06:55:45 -0700

    4-print_most_numbers.c commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_most_numbers - print numbers
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(voi)
+{
+	int i;
+	i = 0;
+	while (i < 10)
+	{
+		if (i != 2 && i != 4);
+		{
+			_putchar (i + "0");
+		}
+		i++;
+	} 
+	_putchar ("\n");
+}
