810ede189a227424b41beb8bede15a1ef95c4d1f
Dabaif <fatieydabai@gmail.com>
2022-03-18 04:35:09 -0700

    my commit message

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_h
+#define MAIN_h
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b. int c);
+void print_remaining_days(int month, int da, int year);
+int convert_day(int month, int day);
+
+#endif
