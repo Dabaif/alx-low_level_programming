8e3f8c44f0bf0c154f66426d9daef9943853d0a8
Dabaif <fatieydabai@gmail.com>
2022-03-22 12:06:50 -0700

    v

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,39 +1,29 @@
-#include "main.h"
-
+#include <stdio.h>
+#include "holberton.h"
 /**
-* rev_string - update value
-* @s: value to be evaluated
-* Return: void when correct
+* rev_string - update value.
+* @s: value to be evaluate.
+* Return: not.
 */
 void rev_string(char *s)
 {
-	char *start_c, *end_c, c;
-	int i, count;
-	int length = 0;
-
-for (i = 0; s[i]; i++)
-{
-	length++;
-}
-
-count = length;
-
-start_c = s;
-end_c = s;
-
-
-for (i = 0; i < count - 1; i++)
-{
-	end_c++;
-}
-
-for (i = 0; i < count / 2; i++)
-{
-
-	c = *end_c;
-	*end_c = *start_c;
-
-	start_c++;
-	end_c--;
-}
+	int len = 0;
+	int l = 0;
+	char *y = s;
+	int e = 0;
+	int x;
+	char n;
+	while (*y != '\0')
+	{
+		y++;
+		len++;
+	}
+	l = len - 1;
+	for ( ; e < ((l / 2) + 1) ; e++)
+	{
+		x = (l - e);
+		n = s[e];
+		s[e] = s[x];
+		s[x] = n;
+	}
 }
