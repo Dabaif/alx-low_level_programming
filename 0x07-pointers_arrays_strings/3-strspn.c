4fe577c92f8814be049dfcc8a894e9174e290421
Dabaif <fatieydabai@gmail.com>
2022-03-29 14:48:12 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* _strspn - length of a prefix substring
+* @s: string to check
+* @accept: string to check against
+* Return: number of bytes of s in accept
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, bool;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		bool = 1;
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				bool = 0;
+				break;
+			}
+		}
+		if (bool == 1)
+			break;
+	}
+	return (i);
+}
